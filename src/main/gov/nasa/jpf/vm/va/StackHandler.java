9aa89f4f6a9bb4917976e2a775f1bc457c4b3606
Meng Meng <mengmeng2811@gmail.com>
2015-11-25 21:57:53 -0800

    Merge to master

--- src/main/gov/nasa/jpf/vm/va/StackHandler.java
+++ src/main/gov/nasa/jpf/vm/va/StackHandler.java
@@ -45,10 +45,10 @@ public class StackHandler implements Cloneable, IStackHandler {
 	public FeatureExpr stackCTX;
 	
 	
-	//*** dataCollect class object***//
-	public DataCollect c = new DataCollect(this);
+	//*** StackHandlerTracker class object***//
+	public StackHandlerTracker c = new StackHandlerTracker();
 	
-	static LinkedList<DataCollect> q = new LinkedList<DataCollect>();
+	//static LinkedList<StackHandlerTracker> q = new LinkedList<StackHandlerTracker>();
 	
 	
 	/* (non-Javadoc)
@@ -84,7 +84,6 @@ public class StackHandler implements Cloneable, IStackHandler {
 
 	@SuppressWarnings("unchecked")
 	public StackHandler(FeatureExpr ctx, int nLocals, int nOperands) {
-		c.setMin(nOperands);
 		if (ctx == null) {
 			throw new RuntimeException("CTX == NULL");
 		}
@@ -93,7 +92,7 @@ public class StackHandler implements Cloneable, IStackHandler {
 		Arrays.fill(locals, nullValue);
 		stack = new One<>(new Stack(nOperands, this.c));
 		stackCTX = ctx;
-		q.addLast(this.c);
+		//q.addLast(this.c);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -439,7 +438,7 @@ public class StackHandler implements Cloneable, IStackHandler {
 				return ChoiceFactory.create(ctx, new One<>(clone), new One<>(stack));
 			}
 		}).simplify();
-		c.push();
+		c.push(this, ctx, value, isRef);
 	}
 
 	/* (non-Javadoc)
@@ -514,7 +513,7 @@ public class StackHandler implements Cloneable, IStackHandler {
 			}
 		}).simplifyValues();
 		stack = stack.simplify();
-		c.pop();
+		c.pop(this, ctx,0);
 		return result;
 	}
 
@@ -542,7 +541,7 @@ public class StackHandler implements Cloneable, IStackHandler {
 				return ChoiceFactory.create(f, new One<>(clone), new One<>(s));
 			}
 		}).simplify();
-		c.pop();
+		c.pop(this, ctx, n);
 	}
 
 	/* (non-Javadoc)
@@ -847,6 +846,7 @@ public class StackHandler implements Cloneable, IStackHandler {
 	@Override
 	public void dup(final FeatureExpr ctx) {
 		function(ctx, StackInstruction.DUP);
+		c.dup(this, ctx);
 	}
 
 	/* (non-Javadoc)
@@ -910,7 +910,6 @@ public class StackHandler implements Cloneable, IStackHandler {
 				return ChoiceFactory.create(ctx, new One<>(clone), new One<>(stack));
 			}
 		}).simplify();
-		c.dup();
 	}
 
 	@Override
--- src/main/gov/nasa/jpf/vm/va/Test.java
+++ src/main/gov/nasa/jpf/vm/va/Test.java
@@ -5,6 +5,7 @@ package gov.nasa.jpf.vm.va;
 import gov.nasa.jpf.vm.MJIEnv;
 import gov.nasa.jpf.vm.Types;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -18,12 +19,15 @@ import cmu.conditional.MapChoice;
 import cmu.conditional.VoidBiFunction;
 import de.fosd.typechef.featureexpr.FeatureExpr;
 import de.fosd.typechef.featureexpr.FeatureExprFactory;
-
+import java.util.logging.*;
+import java.util.logging.Logger;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 
 public class Test {
-
-    public static void main(String[] args) {
-    	
+   
+    public static void main(String[] args) throws IOException {
+    
     	
         FeatureExpr ta, tb, tc;
     	
@@ -49,18 +53,26 @@ public class Test {
 		Conditional<Integer> F = ChoiceFactory.create(tb, G, new One<>(10));
 		
 		
-		//stack.push(FeatureExprFactory.True(), A, true);
 		stack.push(FeatureExprFactory.True(), B, false);
 		stack.push(FeatureExprFactory.True(), C, true);
 		stack.push(FeatureExprFactory.True(), D, false);
+		stack.pop(FeatureExprFactory.True());
 		stack.push(FeatureExprFactory.True(), E, false);
-		//stack.push(FeatureExprFactory.True(), F, false);
-		//stack.push(FeatureExprFactory.True(), G, false);
-        //stack.dup2_x2(ctx);
-		stack.push(ta, B, false);
-		stack.pop(ta);
+		stack.push(ta.not(), D, false);
 		stack.dup(ta.not());
 		stack.dup(ta.not());
+		stack.pop(ta.not());
+		stack.pop(ta, 2);
+		
+	//	System.out.println(StackHandler.q.toString());
+		stack.push(tb, D);
+		
+		
+		
+//		System.out.println(StackHandler.q.toString());
+//		System.out.println(stack.toString());
+		
+		
 //		IStackHandler stack2 = StackHandlerFactory.createStack(FeatureExprFactory.True(), 0, 10);
 //		stack2.push(FeatureExprFactory.True(), A, true);
 //		stack2.push(FeatureExprFactory.True(), B, false);
@@ -69,14 +81,9 @@ public class Test {
 		//stack.push(FeatureExprFactory.True(), E, false);
 		//stack2.push(FeatureExprFactory.True(), F, false);
 		
-		
-		
+		//System.out.println(stack);
         //System.out.println(stack.getTop());
 		//System.out.println(stack.numOP().toString());
-		System.out.println(StackHandler.q.toString());
-		//System.out.println(stack);
-	
-		
 		//System.out.println(stack2.getTop());
 		//System.out.println(stack2.numOP().toString());
 		
